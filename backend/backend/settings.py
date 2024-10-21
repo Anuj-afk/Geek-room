118b50bf7d9653ef8f25ed88170f7bebfad64c82
Anuj Gupta <65494292+Anuj-afk@users.noreply.github.com>
2024-10-22 03:19:06 +0530

    changed database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -111,15 +111,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 import dj_database_url
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
-#     'default': dj_database_url.parse(os.environ['DATABASE_URL'])
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
+# }
+DATABASES = {
+    'default': dj_database_url.parse(os.environ['DATABASE_URL'])
+    }
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
